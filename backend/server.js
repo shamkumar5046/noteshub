552ff9190b5db93306dfe761cec7d4e270bfc3e4
sham kumar.s <shamaiml@sriramec.edu.in>
2026-01-02 22:02:37 +0530

    Add production CORS origin for deployment

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
+  'https://sriram-edtech.onrender.com',
 ];
 
 // In development, allow any localhost origin
